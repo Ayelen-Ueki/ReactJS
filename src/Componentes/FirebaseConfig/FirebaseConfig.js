903d407dc7bbd5fb2322666817471a2e5cd989dd
Maria Ayelen Ueki <aye_ueki_011@hotmail.com>
2022-03-01 16:02:25 -0300

    Firebase

---
+++ src/Componentes/FirebaseConfig/FirebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBafTwR-98nzkiwaGjWvWUxtaBHcxd4Lm0",
+  authDomain: "feelinpasteleria.firebaseapp.com",
+  projectId: "feelinpasteleria",
+  storageBucket: "feelinpasteleria.appspot.com",
+  messagingSenderId: "588262740217",
+  appId: "1:588262740217:web:9f0f511f69009f105e9228"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore (app);
+
